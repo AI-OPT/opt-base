151d9234c16a9ec0c734fdad0384634389bc5c9d
gucl <gucl@asiainfo.com>
2016-11-02 15:35:11 +0800

    恢复1.0.1-SNAPSHOT

--- src/main/java/com/ai/opt/base/vo/BaseInfo.java
+++ src/main/java/com/ai/opt/base/vo/BaseInfo.java
@@ -1,33 +1,16 @@
 package com.ai.opt.base.vo;
 
 import java.io.Serializable;
-import java.util.Locale;
-import java.util.TimeZone;
-
-import org.springframework.context.i18n.LocaleContextHolder;
-
-import com.ai.paas.ipaas.i18n.ZoneContextHolder;
 
 /**
  * 参数基础类.
- * Date: 2016年11月1日 <br>
- * Copyright (c) 2016 asiainfo.com <br>
+ * Date: 2015年10月19日 <br>
+ * Copyright (c) 2015 asiainfo.com <br>
  * 
- * @author gucl
+ * @author zhangchao
  */
 public class BaseInfo implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
-	public BaseInfo() {
-		// set in the web
-		if (null != LocaleContextHolder.getLocale()) {
-			locale = LocaleContextHolder.getLocale();
-		}
-		if (null != ZoneContextHolder.getZone()) {
-			timeZone = TimeZone.getTimeZone(ZoneContextHolder.getZone());
-		}
-	}
-	
 	/**
 	 * 租户Id，必填
 	 */
@@ -36,28 +19,7 @@ public class BaseInfo implements Serializable{
 	 * 租户密码，可选
 	 */
 	private String tenantPwd;
-	
-	/**
-	 * 区域属性
-	 */
-	private Locale locale;
 
-	private TimeZone timeZone;
-	
-	
-
-	public Locale getLocale() {
-		return locale;
-	}
-	public void setLocale(Locale locale) {
-		this.locale = locale;
-	}
-	public TimeZone getTimeZone() {
-		return timeZone;
-	}
-	public void setTimeZone(TimeZone timeZone) {
-		this.timeZone = timeZone;
-	}
 	public String getTenantId() {
 		return tenantId;
 	}
